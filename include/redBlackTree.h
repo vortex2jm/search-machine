7b191f94d1bd213254068c0caec753d80e1bf443
vortex2jm <joaopaulomoura5328@gmail.com>
2023-06-24 20:14:21 -0300

    :memo: Commenting parts heap parts

--- include/redBlackTree.h
+++ include/redBlackTree.h
@@ -1,17 +1,16 @@
 #ifndef RBT_H
 #define RBT_H
 
+#include "page.h"
 #include <stdbool.h>
 #include <string.h>
 
-#include "../include/binaryHeap.h"
-
 typedef struct node RBT;
 
-RBT* rbtCreateNode(char* word, Heap* pages, bool color, int maxPages);
+// RBT* rbtCreateNode(char* word, Heap* pages, bool color, int maxPages);
 
-Heap* rbtSearch(RBT* root, char* key);
+// Heap* rbtSearch(RBT* root, char* key);
 
-RBT* RBT_insert(RBT *node, char* key, Page* value, int maxPages);
+RBT *RBT_insert(RBT *node, char *key, Page *value, int maxPages);
 
 #endif
