e46643f5a024976412b7b53ba6d41eb8b99578a6
vortex2jm <joaopaulomoura5328@gmail.com>
2023-06-24 20:14:53 -0300

    :sparkles: Initializing util lib

---
+++ src/util.c
@@ -0,0 +1,18 @@
+#include "../include/util.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//================//
+void pageRanking() {
+  // TODO
+}
+
+//================//
+void indexer(){
+  // TODO
+}
+
+//================//
+void consult(){
+  // TODO
+}
