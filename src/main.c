be4440eaa23c442f22651ca75adf42f24dd23406
vortex2jm <joaopaulomoura5328@gmail.com>
2023-06-24 20:11:55 -0300

    :sparkles: Getting and validating args

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+#include "../include/page.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello world");
+
+  // Validation =============//
+  if (argc < 2) {
+    printf("You must insert main folder name!\n");
+    return 1;
+  }
+
+  // Data reading============//
+  char *mainDir = strdup(argv[1]);
+
+  // Data processing=========//
+
+  // Consult reading=========//
+
+  // Consult processing======//
+
+  free(mainDir);
+
   return 0;
 }
