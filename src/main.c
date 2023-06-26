b5d9c8cc0f6d0cffd1fc21204a036ab0447b414c
vortex2jm <joaopaulomoura5328@gmail.com>
2023-06-25 21:00:02 -0300

    :sparkles: Adding reading pages features

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include "../include/page.h"
+#include "../include/util.h"
+#include "../include/redBlackTree.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,12 +9,14 @@ int main(int argc, char *argv[]) {
 
   // Validation =============//
   if (argc < 2) {
-    printf("You must insert main folder name!\n");
+    printf("You must insert main directory name!\n");
     return 1;
   }
 
   // Data reading============//
   char *mainDir = strdup(argv[1]);
+  Tree * pageTree = readPages(mainDir);
+  treeTraversalInOrder(pageTree, printPage);  // For debug
 
   // Data processing=========//
 
