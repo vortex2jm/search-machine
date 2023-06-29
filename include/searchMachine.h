47c2035bbef6636cf553c1529441a2ad88da8cee
Henrique <hecaetano1@hotmail.com>
2023-06-29 20:24:13 -0300

    :sparkles: starting search processor

--- include/searchMachine.h
+++ include/searchMachine.h
@@ -7,7 +7,8 @@
 // Calcula o page ranking das páginas que estão na árvore de páginas
 void pageRanking(int pageAmount, pagesTree *pageTree);
 
-// void indexer();
-// void consult();
+//recebe termos a serem pesquisados e obtém as páginas relevantes, em ordem
+void searchProcessor(termsTree* terms, stopWordTree* stopWords, char* mainDir);
+
 
 #endif // !SEARCH_MACHINE
